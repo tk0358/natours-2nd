e93e305c94478812e5d857dfb5721ed7ed32a243
tk0358 <tk0358a@yahoo.co.jp>
2020-11-26 15:34:31 +0900

    Add ndb for debugging & handle unhandled routes

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
 
 // 1) MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -23,8 +20,14 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -14,7 +14,7 @@ class APIFeatures {
 
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    // console.log(JSON.parse(queryStr));
 
     this.query = this.query.find(JSON.parse(queryStr));
 
