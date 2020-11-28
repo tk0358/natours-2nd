f3e07fc9c76bb8df4db45e56f0f5964dee3d9205
tk0358 <tk0358a@yahoo.co.jp>
2020-11-28 21:38:03 +0900

    Implement catch 'unhandledRejection' & 'uncaughtException' error handling

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 // console.log(process.env.NODE_ENV);
@@ -20,6 +26,15 @@ mongoose
 
 // 4) START SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
