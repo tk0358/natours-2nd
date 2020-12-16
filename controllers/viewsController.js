4de04865a5a4416bf4624a5531aa97af9f7faad1
tk0358 <tk0358a@yahoo.co.jp>
2020-12-16 12:08:33 +0900

    Implement updateSettings at views

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
-const AppError = require('../utils/appError');
-const Review = require('../models/reviewModel');
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
+// const Review = require('../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res) => {
@@ -49,3 +49,28 @@ exports.getLoginForm = (req, res) => {
       title: 'Log into your account',
     });
 };
+
+exports.getAccount = (req, res) => {
+  res.status(200).render('account', {
+    title: 'Your account',
+  });
+};
+
+exports.updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser,
+  });
+});
